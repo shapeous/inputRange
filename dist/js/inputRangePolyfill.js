104a528d1c0892f678510a5c9b69c2cd76867bc2
Shapeous <shapeous@yahoo.com>
2015-07-17 11:30:04 -0400

    Adjust so that it works on IE9

--- dist/js/inputRangePolyfill.js
+++ dist/js/inputRangePolyfill.js
@@ -1,24 +1,38 @@
 // inputRangePolyfill.js
-// Author: Shapeous
+// Repo: https://github.com/shapeous/inputRange
 
 // Get all inputs
 var input = document.getElementsByTagName("input");
 
 for (var i = 0; i < input.length; i += 1) {
-  if (input[i].type === "range" && input[i].hidden !== true) {
-    // Find all non hidden inputs of type range
-    var range = {"min":input[i].min,"max":input[i].max,"value":input[i].value};
-    // hide as found
-    input[i].hidden = true;
-    // find the span
+  // Does the current input meet the following criteria?
+  if (  input[i].attributes.type.value=== "range"
+    &&  input[i].attributes.hidden    === undefined
+    &&  input[i].attributes.disabled  !== undefined
+    &&  input[i].getAttribute("min")  !== undefined
+    &&  input[i].getAttribute("value")!== undefined
+    &&  input[i].getAttribute("max")  !== undefined
+    ) { // if so
+    // That's our kind of range lets save it
+    var range =
+    { "min":  parseInt(input[i].getAttribute("min"),  10)
+    , "value":parseInt(input[i].getAttribute("value"),10)
+    , "max":  parseInt(input[i].getAttribute("max"),  10)
+    };
+    // Let's also hide it so we can draw our polyfill
+    input[i].setAttribute("hidden");
+    // Let's also get its neighboring span
+    // (right now we need to have one to the right of the range) and so todo: Check if it exist first maybe even add it...
     var span = input[i].nextSibling;
-    // draw the span range
+    // Let's add a class name so we can style it
+    span.setAttribute("class", "range");
+    // draw the polyfill
     var str = "";
-    for (var j = range.min; j < range.value; j++) {
+    for (var j = range.min; j < range.value; j += 1) {
       str = str.concat("=");
     }
     str = str.concat("[]");
-    for (var k = range.value; k < range.max; k++) {
+    for (var k = range.value; k < range.max; k += 1) {
       str = str.concat("=");
     }
     span.textContent = str;
